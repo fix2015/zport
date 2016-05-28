3153d485fc222a554d66423d5cecd957e415a1c1
semianchuk <fix20152@gmail.com>
2016-05-28 13:50:14 +0300

    fix

--- components/contacts/Contacts.js
+++ components/contacts/Contacts.js
@@ -17,7 +17,6 @@ var Contacts = React.createClass({
 componentDidMount(){
     VK.Widgets.Group("vk_groups", {mode: 0, width: "275", height: "400"}, 14959699);
 },
-
 render() {
     return (
         <div>
@@ -26,7 +25,7 @@ render() {
             </div>
             <div className="col-md-12">
                 <div className="col-md-8 text-left">
-                    <h3 className="text-center">Контакты</h3>
+                    <h3 className="text-center"> Контакты </h3>
                     <p>Мы предлагаем вам сайт который станет хорошим помошником в поиске жилья.
                     Сайт находится на стадии разработки с каждным днем мы будем его делать все лучше и лучше для Вас.</p>
                     <p>Предложения и пожелания по сайту пишите на почту <a href="mailto:fix20152@gmail.com">fix20152@gmail.com</a></p>
@@ -37,7 +36,7 @@ render() {
                 </div>
                 <div className="col-md-4">
                     <div id="vk_groups"></div>
-                    <span className="myName"><h4>По вопросам сотрудничества обращайтесь по тел. +380507612588</h4></span>
+                    <span className="myName"><h4>По вопросам сотрудничества обращайтесь по телефону +380507612588</h4></span>
                 </div>
             </div>
             <div className="col-md-12">
